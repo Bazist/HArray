b0eff166192fa4ba47fee965ee3a27bf5fe66629
bazist <bazist@github.com>
2016-11-06 16:33:30 -0500

    copyrights

--- HArray/HArrayFixBase.h
+++ HArray/HArrayFixBase.h
@@ -1,3 +1,21 @@
+/*
+# Copyright(C) 2010-2016 Vyacheslav Makoveychuk (email: slv709@gmail.com, skype: vyacheslavm81)
+# This file is part of VyMa\Trie.
+#
+# VyMa\Trie is free software : you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Vyma\Trie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #ifndef _HARRAY_FIX_BASE		 // Allow use of features specific to Windows XP or later.
--- HArray/stdafx.h
+++ HArray/stdafx.h
@@ -21,29 +21,6 @@
 // are changed infrequently
 //
 
-/*
-# Copyright(C) 2010-2016 Vyacheslav Makoveychuk (email: slv709@gmail.com, skype: vyacheslavm81)
-# This file is part of VyMa\Trie.
-#
-# VyMa\Trie is free software : you can redistribute it and / or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Vyma\Trie is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.

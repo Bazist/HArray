9b98609f1bf050eca81c0f2bb19052af9945a747
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2020-11-01 19:01:38 +0200

    fix project name in license

--- HArray/Main.cpp
+++ HArray/Main.cpp
@@ -1,13 +1,13 @@
 ﻿/*
 # Copyright(C) 2010-2017 Viacheslav Makoveichuk (email: slv709@gmail.com, skype: vyacheslavm81)
-# This file is part of VyMa\Trie.
+# This file is part of HArray.
 #
-# VyMa\Trie is free software : you can redistribute it and / or modify
+# HArray is free software : you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Vyma\Trie is distributed in the hope that it will be useful,
+# HArray is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 # GNU General Public License for more details.

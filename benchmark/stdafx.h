295ef11825df7d00a913d88089a6594b3fbef660
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2021-07-18 21:58:12 +0300

    fix Ubuntu build error with stdafx.h

---
+++ benchmark/stdafx.h
@@ -0,0 +1,38 @@
+/*
+# Copyright(C) 2010-2017 Viacheslav Makoveichuk (email: slv709@gmail.com, skype: vyacheslavm81)
+# This file is part of HArray.
+#
+# HArray is free software : you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HArray is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#include <cstdint>
+#include <string>
+#include <cstring>
+#include <limits>
+
+#include <stdio.h>
+#include <inttypes.h>
+
+// TODO: reference additional headers your program requires here
